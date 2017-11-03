b6a00ef42dc33ac045187627c3d92a9374f26b33
kevin-fu <kevin-fu@sjtu.edu.cn>
2017-11-03 20:29:30 +0800

    2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner =
+//        Scanner scanner =
         System.out.println("Hello World!");
     }
 }
